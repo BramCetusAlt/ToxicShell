46bc9bf6a6ce05b3b3909fc210bb8c38ea894490
George Rellias <39456857+BramCetusAlt@users.noreply.github.com>
2018-06-15 14:33:57 +0300

    Add files via upload

---
+++ ToxicShell.py
@@ -0,0 +1,20 @@
+import subprocess, os
+
+run = True
+
+while run == True:
+    try:
+        command = str(input("ToxicShell:>"))
+        if command == "exit" or command == "Exit":
+            run = False
+        elif command[:2] == "cd":
+            os.chdir(command[3:])
+        else:
+            process = subprocess.Popen(command[:], stdout = subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
+            output = process.stdout.read() + process.stderr.read()
+            output_string = str(output)
+            print(str(output))
+    except Exception as msg:
+        print("An error occured! ", str(msg))
+    
+    
